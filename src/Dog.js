ed8c8dc832d8660e872db1970e9fc22acc90c95b
Gamoundo <akilgg@gmail.com>
2021-04-01 21:10:42 -0400

    setup the dog api fetch

--- src/Dog.js
+++ src/Dog.js
@@ -1,11 +1,41 @@
 import React from 'react';
 
-function Dog () {
-    return (
-        <div className="factDisplay">
-            <p> Dogs are great</p>
-        </div>
-    )
+
+class Dog extends React.Component{
+
+    state = {
+        dogFacts: []
+       }
+       
+       componentDidMount() {
+           fetch("https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/?number=5")
+           .then(resp => resp.json())
+           .then(dogFacts => this.setState({ dogFacts}))
+           
+         }
+         displaydogFacts = (array) => {
+           return (array.map((dogFact) => {
+               return (
+                   <div className="facts">
+                       <p>{dogFact.fact}</p>
+                   </div>
+               )
+           }))
+       }
+       
+       render(){
+           console.log(this.state.dogFacts)
+        return (
+            <div className="factDisplay">
+                 {this.displaydogFacts(this.state.dogFacts)}
+            </div>
+        )
+       }
+           
+
 }
+
+
+
 
 export default Dog;
