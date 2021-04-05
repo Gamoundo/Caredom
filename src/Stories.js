e371f80f296a90043d2f749f71b3d72f73304ffb
Gamoundo <akilgg@gmail.com>
2021-04-04 20:39:39 -0400

    made a function to split the text

--- src/Stories.js
+++ src/Stories.js
@@ -5,12 +5,27 @@ import React from 'react';
 
 function Stories (props) {
     console.log(props)
+    const splitAndDisplayText = (str) => {
+        const  sentences = str.split(".")
+        console.log(sentences)
+          return (sentences.map((sentence) => {
+              return(
+            <div className="sentence">
+                  <p > {sentence}.</p>
+              </div>
+              )
+              
+          
+          }
+          ))
+      }
     const displayCatFacts = (array) => {
+       
         return (array.map((animal) => {
             return (
                 <div className="stories">
                     <img src={animal.img} alt ={animal.species} width ="300" height ="300"/>
-                    <p>{animal.text}</p>
+                    {splitAndDisplayText(animal.text)}
                 </div>
             )
         }))
